f809702b835d5bc6e5fd57a6f2fb154b576fcbcf
dkumar2701 <divykumar2023@u.northwestern.edu>
2023-04-04 12:59:08 -0500

    more tests

--- test.py
+++ test.py
@@ -39,6 +39,82 @@ if len(start_array)==len(final_array):
 		print("stats failed")
 else:
 	print("size test failed")
+	
+start_array = [1, 0]
+final_array = [0, 1]
+start_stats = [0, 0, 0]
+final_stats = [0, 0, 0]
+
+print("\n\rrunning test 6")
+print("input data",start_array)
+student_code.order(start_array,start_stats)
+print("student sorted data",start_array)
+print("expected data", final_array)
+print("student stats",start_stats)
+print("expected stats", final_stats)
+grade=2
+
+if len(start_array)==len(final_array):
+	tests_passed=tests_passed+1
+	print("size test passed")
+	grade=1
+	for x in range(len(start_array)):
+		if start_array[x]!=final_array[x]:
+			grade=0
+	if grade==1:
+		print("sorted array passed")
+		tests_passed=tests_passed+1
+	else:
+		print("sorted array failed")
+	grade=1
+	for x in range(len(start_stats)):
+		if start_stats[x]!=final_stats[x]:
+			grade=0
+	if grade==1:
+		print("stats passed")
+		tests_passed=tests_passed+1
+	else:
+		print("stats failed")
+else:
+	print("size test failed")
+	
+start_array = [0, 1, 2]
+final_array = [0, 1, 2]
+start_stats = [0, 0, 0]
+final_stats = [1, 1, 0]
+
+print("\n\rrunning test 6")
+print("input data",start_array)
+student_code.order(start_array,start_stats)
+print("student sorted data",start_array)
+print("expected data", final_array)
+print("student stats",start_stats)
+print("expected stats", final_stats)
+grade=2
+
+if len(start_array)==len(final_array):
+	tests_passed=tests_passed+1
+	print("size test passed")
+	grade=1
+	for x in range(len(start_array)):
+		if start_array[x]!=final_array[x]:
+			grade=0
+	if grade==1:
+		print("sorted array passed")
+		tests_passed=tests_passed+1
+	else:
+		print("sorted array failed")
+	grade=1
+	for x in range(len(start_stats)):
+		if start_stats[x]!=final_stats[x]:
+			grade=0
+	if grade==1:
+		print("stats passed")
+		tests_passed=tests_passed+1
+	else:
+		print("stats failed")
+else:
+	print("size test failed")
 
 
 
